5808a9463e99b74c8b5bef5a648814af89b12de2
Srinivasan <srinivasanvelu23@gmail.com>
2021-11-05 23:20:33 +0530

    hi

---
+++ pages/products/rapiscan622xr/index.tsx
@@ -0,0 +1,110 @@
+import React from 'react';
+import { Breadcrumb } from 'react-bootstrap';
+import { NavBarHeader } from '../../../components/navbar';
+import styles from './rap622xr.module.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCheck, faCheckDouble, faCircle } from '@fortawesome/free-solid-svg-icons';
+import { FooterWeb } from '../../../components/footer';
+import styles1 from '../../../components/footer/footer.module.css';
+const rap622xr = () =>{
+    return(<><NavBarHeader></NavBarHeader>
+        <section className={styles.breadcrumbs}>
+            <div className="container">
+
+                <div className="d-flex justify-content-between align-items-center">
+                    <h2 className={styles.headingH2}>622XR</h2>
+                    <Breadcrumb>
+                        <Breadcrumb.Item href="#">Home</Breadcrumb.Item>
+                        <Breadcrumb.Item href="#">Products</Breadcrumb.Item>
+                        <Breadcrumb.Item active>baggage and parcel inspection</Breadcrumb.Item>
+                        <Breadcrumb.Item active>622XR</Breadcrumb.Item>
+                    </Breadcrumb>
+                </div>
+
+            </div>
+        </section>
+        <section id="about" className="about">
+      <div className="container aos-init aos-animate" data-aos="fade-up">
+
+   
+
+        <div className="row my-4">
+          <div className="col-lg-4">
+           
+            <img src="/images/products/5X.jpg" className="img-fluid pt-5" alt=""/></div>
+          
+          <div className="col-lg-8 pt-4 pt-lg-0 content">
+            <p className={styles.productdescr}>Feature-rich X-ray baggage inspection system for screening large carry-on baggage and parcels at the checkpoint.</p>
+            <p  className={styles.productdescr}>
+            The 622XR offers high performance and explosive threat and narcotics detection alert. With a tunnel opening of 750mm x 550mm, the 622XR can  accommodate a range of parcel and baggage sizes for checkpoints.</p>
+            <p  className={styles.productdescr}>
+            The 622XR’s innovative design allows for bidirectional operation, while its dual-energy detection provides material discrimination alert so that screeners can identify the material composition of the scanned object. In addition, it offers options like an adjustable control panel stand and swing-open, easy-access panels. 
+            </p>
+            
+            <div className="row">
+              <div className="col-lg-12">
+              <ul className={`${styles.productslist} list-group list-group-flush`}>
+    <li className="list-group-item"><FontAwesomeIcon icon={faCheckDouble} className="text-primary"  /> <span> LARGE BAGGAGE AND PARCEL SCREENING</span></li>
+    <li className="list-group-item"><FontAwesomeIcon icon={faCheckDouble}  className="text-primary" /><span> APPROVED BY MAJOR AVIATION REGULATORY AUTHORITIES</span></li>
+    <li className="list-group-item"><FontAwesomeIcon icon={faCheckDouble}  className="text-primary" /><span> EXPLOSIVE AND NARCOTICS DETECTION ALERT</span></li>
+    <li className="list-group-item"><FontAwesomeIcon icon={faCheckDouble}  className="text-primary" /><span> NETWORK ENABLED</span></li>
+    </ul>    
+ 
+              </div>
+    
+            </div>
+   
+          </div>
+        </div>
+
+      </div>
+    </section>
+    <section>
+        <div className="container">
+            
+            <div className="row">
+              <div className="col-lg-6 col-md-6 col-12">
+              <h3 className={styles.aboutH3}>DETECTION OF EXPLOSIVES AND NARCOTICS ALERT</h3>
+              <ul  className={styles.listPara}>
+                <li>Target and NARCScan are designed to assist operators in the detection of a range of explosives and narcotics respectively in real time during the scanning process by marking a potential threat on the X-ray image. Rapiscan® Systems detection algorithms are based on regulatory material analysis techniques.</li>
+              </ul>
+              </div>
+              <div className="col-lg-6 col-md-6 col-12">
+              <h3 className={styles.aboutH3}>EASE OF USE PROVIDING HIGHEST THROUGHPUT</h3>  
+              <ul  className={styles.listPara}>
+	<li> With over 13 image processing tools and detection alert algorithm functions, the feature rich software allows the operator to more easily and accurately search for contraband.</li>
+</ul>
+                </div>
+            </div>
+            <div className="row">
+              <div className="col-lg-6 col-md-6 col-12">
+              <h3 className={styles.aboutH3}>REGULATORY COMPLIANCE</h3>
+              <ul  className={styles.listPara}>
+                <li>622XR performance is in full compliance with the European Union regulations (EC) No. 300/2008, (EU) No. 2015/1998 and Commissions Decision C (2015)8005 for aviation security.</li>
+              </ul>
+              </div>
+              
+            </div>
+        </div>
+    </section>
+    <footer id={styles1.footer}>
+        <div className={styles1.footerTop}>
+          <div className="container">
+            <div className="row">
+              <FooterWeb></FooterWeb>
+              <div className={styles1.container}>
+                <div className={styles1.copyright}>
+                  © 2021 <strong><span>ECIL Rapiscan Limited.</span></strong> All Rights Reserved
+                </div>
+
+              </div>
+            </div>
+          </div>
+        </div>
+      </footer>
+        
+        </>)
+};
+
+export default rap622xr;
